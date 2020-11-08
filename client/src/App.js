cd29a7829cc259c1e70700faeccad37e0224e2fa
cdsalty <soltiscd@gmail.com>
2020-11-08 16:05:49 -0500

    fixed styling issues, rdy to move 4ward

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,17 @@ import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import memories from './images/memories.png';
+import useStyles from './styles'
 
 const App = () => {
+
+  const classes = useStyles();
+
   return (
     <Container maxWidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">Memories</Typography>
-
-        {/* The image is squashed; if I remove height, it corrects itself */}
-
-        <img src={memories} alt="shaking hands" height="60" />
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+        <img className={classes.image} src={memories} alt="icon" height="60" />
       </AppBar>
       <Grow in>
         <Container>
@@ -32,4 +33,3 @@ const App = () => {
 }
 
 export default App;
-
