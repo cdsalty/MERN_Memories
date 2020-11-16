91fdeef890ed04e18e457e7aa46904e0bd3b6006
cdsalty <soltiscd@gmail.com>
2020-11-16 16:07:56 -0500

    issue resolved

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -6,7 +6,7 @@ export const getPosts = async (req, res) => {
   try {
     // retrieve the posts in database(will take TIME)
     const postMessages = await PostMessage.find();
-    res.send(200).json(postMessages);
+    res.status(200).json(postMessages);
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
